c00ce8e213a425a224b988f246a99b1073bb59e0
Koding <aneelkkhatri@aneelkkhatri.koding.io>
2016-02-20 05:29:27 -0600

    wrapper api initialized

---
+++ app/src/api/index.js
@@ -0,0 +1,20 @@
+var sampleData = require('./sample_data.js');
+
+module.exports = {
+    auth: {
+        login: function (data) {
+        
+        },
+        register: function (data) {
+            
+        }
+    },
+
+    logout: function () {
+        
+    },
+    
+    getSampleData: function () {
+        return sampleData;
+    }
+};
---
+++ app/src/components/App.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var App = React.createClass({
+    render: function () {
+        return <div className="app">
+            <header />
+            <footer />
+        </div>;
+    }
+});
+
+module.exports = App;
