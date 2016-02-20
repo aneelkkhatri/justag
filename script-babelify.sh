6627fe48400a90e09d3556b11268e14a2bcb95b6
aneelkkhatri <anilkumarkhatri@gmail.com>
2016-02-20 16:10:27 +0500

    fix

--- script-babelify.sh
+++ script-babelify.sh
@@ -1,2 +1,1 @@
-./node_modules/.bin/babel --presets react,es2015 --plugins transform-object-rest-spread --watch app/src --out-dir app/b
-uild
+./node_modules/.bin/babel --presets react,es2015 --plugins transform-object-rest-spread --watch app/src --out-dir app/build
