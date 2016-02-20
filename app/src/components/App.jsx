488515411ced5cb40863c22b375e067b3be6413f
aneelkkhatri <anilkumarkhatri@gmail.com>
2016-02-20 18:57:41 +0500

    basic ui

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -4,6 +4,59 @@ var App = React.createClass({
     render: function () {
         return <div className="app">
             <header />
+            <div className="main-container">
+            	<div className="left-bar">
+            		<div className="top">
+        				<div></div>
+            		</div>
+            		<div className="mid">
+            			<ul>
+            				<li>item 1</li>
+            				<li>item 2</li>
+            				<li>item 3</li>
+            				<li>item 4</li>
+            				<li>item 1</li>
+            				<li>item 2</li>
+            				<li>item 3</li>
+            				<li>item 4</li>
+            				<li>item 1</li>
+            				<li>item 2</li>
+            				<li>item 3</li>
+            				<li>item 4</li>
+            				<li>item 1</li>
+            				<li>item 2</li>
+            				<li>item 3</li>
+            				<li>item 4</li>
+            				<li>item 1</li>
+            				<li>item 2</li>
+            				<li>item 3</li>
+            				<li>item 4</li>
+            				<li>item 1</li>
+            				<li>item 2</li>
+            				<li>item 3</li>
+            				<li>item 4</li>
+            				<li>item 1</li>
+            				<li>item 2</li>
+            				<li>item 3</li>
+            				<li>item 4</li>
+            				<li>item 1</li>
+            				<li>item 2</li>
+            				<li>item 3</li>
+            				<li>item 4</li>
+            				<li>item 1</li>
+            				<li>item 2</li>
+            				<li>item 3</li>
+            				<li>item 4</li>
+            			</ul>
+            		</div>
+            		<div className="row bottom">
+        				
+            		</div>
+            	</div>
+            	<div className="content">
+	            	Hello, world
+            	</div>
+            </div>
             <footer />
         </div>;
     }
